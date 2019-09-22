f1c491ac915217713e35333f8f6a70f6eef5e1fe
omar hussain <omar.hussain4991@gmail.com>
2019-09-22 18:41:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -16,3 +16,4 @@ function destructivelyAddElementToEndOfArray (array, element) {
 function accessElementInArray (array, index) {
   return (array[2])
 }
+function destructivelyRemoveElementFromBeginningOfArray (array)
