516645eb6ce42538366b048fc925318c11d1e206
omar hussain <omar.hussain4991@gmail.com>
2019-09-22 18:50:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,5 +21,5 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
   return array
 }
 function removeElementFromBeginningOfArray (array) {
-  return array.slice[2, 3]
+  return array.slice[0]
 }
