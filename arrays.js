f0d13b67da858d8aba79ea5288947623113d21ed
omar hussain <omar.hussain4991@gmail.com>
2019-09-22 19:22:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,5 +28,5 @@ function destructivelyRemoveElementFromEndOfArray (array) {
   return array
 }
 function removeElementFromEndOfArray (array) {
-  array.slice(0, array.lenght - 1)
+  return array.slice(0, array.lenght - 1)
 }
